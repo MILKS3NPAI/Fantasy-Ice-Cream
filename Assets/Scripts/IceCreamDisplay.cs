8d8245bf7fc37ff96c5301034adc805a41229880
Ron Riley Co <60789953+MILKS3NPAI@users.noreply.github.com>
2021-02-09 22:22:22 -0800

    -Organized Prefabs -Created Clear function for ice cream display ( Press Q)

--- Assets/Scripts/IceCreamDisplay.cs
+++ Assets/Scripts/IceCreamDisplay.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class IceCreamDisplay : MonoBehaviour
 {
+    public GameObject display;
+    public GameObject cone;
     //display ice cream
     public GameObject van;
     public GameObject choco;
@@ -33,6 +35,10 @@ public class IceCreamDisplay : MonoBehaviour
         {
             scoop(straw);
         }
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            Clear();
+        }
     }
 
     void scoop(GameObject x)
@@ -42,5 +48,35 @@ public class IceCreamDisplay : MonoBehaviour
         x.SetActive(true);
         x.transform.localPosition = new Vector2(0, (-90 + (100 * stack)));
         stack++;
+    }
+
+    public void Clear()
+    {
+        int i = 0;
+
+        //Array to hold all child obj
+        GameObject[] allChildren = new GameObject[transform.childCount];
+
+        //Find all child obj and store to that array
+        foreach (Transform child in transform)
+        {
+            allChildren[i] = child.gameObject;
+            i += 1;
+        }
+
+        //Now destroy them
+        foreach (GameObject child in allChildren)
+        {
+            //DestroyImmediate(child.gameObject);
+            child.SetActive(false);
+        }
+        stack = 0;
+
+        cone = Instantiate(cone, new Vector3 (0, 0, 0), Quaternion.identity);
+        cone.transform.SetParent(transform, false);
+        cone.transform.localPosition = new Vector2(0, -190);
+        cone.transform.rotation *= Quaternion.Euler(0, 0, 180);
+        cone.SetActive(true);
+  
     }
 }

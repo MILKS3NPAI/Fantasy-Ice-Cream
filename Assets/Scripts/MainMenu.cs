f58180a173a8ed9fddba825371b2753b49baeed2
KevinK <kevinkubota0@gmail.com>
2021-03-05 23:17:59 -0800

    -Changed UI positions slightly -Added DOTween to assets folder for tweening -Added a UI animation for pressing Play button

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,9 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using DG.Tweening;
 
 public class MainMenu : MonoBehaviour
 {
+    private void Awake()
+    {
+        DOTween.Init(true, true, LogBehaviour.Default);
+    }
+    public void playClick()
+    {
+        DOTween.To(()=> transform.localScale, x=> transform.localScale = x, new Vector3(0, 0, 0), 0.75f);
+        Invoke("playGame", 0.75f);
+    }
+
     public void playGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);

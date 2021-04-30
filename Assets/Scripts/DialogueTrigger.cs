9fd375ededc47776f5ec5a30d9556a2403ffce3b
Ron Riley Co <60789953+MILKS3NPAI@users.noreply.github.com>
2021-04-30 15:38:31 -0700

    -Added intro Scene

---
+++ Assets/Scripts/DialogueTrigger.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    public Dialogue dialogue;
+    bool cStarted = false;
+    public int count = 0;
+
+    public void TriggerDialogue()
+    {
+        FindObjectOfType<Dialogue_Manager>().StartDialogue(dialogue);
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && cStarted == false)
+        {
+            TriggerDialogue();
+            cStarted = true;
+        }
+        else if(Input.GetKeyDown(KeyCode.Space))
+        {
+            FindObjectOfType<Dialogue_Manager>().DisplayNextSentence();
+            count++;
+        }
+    }
+}
